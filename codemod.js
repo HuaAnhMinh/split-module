9821ee827478a0f2987a051c9ab22a26ec8d4126
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-07-06 15:46:49 +0700

    Fix indentation

--- codemod.js
+++ codemod.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 
 module.exports = (fileInfo, { jscodeshift: j }, options) => {
   const source = j(fileInfo.source)
-    .find(j.VariableDeclaration, {
-      declarations: [{
-            type: 'VariableDeclarator',
-            init: {
-              type: 'CallExpression',
-                callee: {
-                  type: 'Identifier',
-                    name: 'require'
-                }
-            }
-        }]
-    })
-    .remove()
-    .toSource();
+  .find(j.VariableDeclaration, {
+    declarations: [{
+      type: 'VariableDeclarator',
+      init: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'require'
+        }
+      }
+    }]
+  })
+  .replaceWith
+  .toSource();
 
   if (typeof options.save === 'string') {
     const saveLocation = path.join(__dirname, options.save);
