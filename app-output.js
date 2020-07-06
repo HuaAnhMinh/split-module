4b53ea451a0ecefce188527d51c14d54d6536f68
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-07-06 16:36:00 +0700

    Can replace module requirement with lazy requirement

--- app-output.js
+++ app-output.js
@@ -1,1 +1,13 @@
+let fs;
+
+function get_fs() {
+  return fs || (fs = require('fs'));
+}
+
+let path;
+
+function get_path() {
+  return path || (path = require('path'));
+}
+
 const number = 1;
--- codemod.js
+++ codemod.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const fs = require('fs');
 
+const functionLazyReplacement = (module, name, isTriggered) => {
+  let func = `function get_${name}() {\n`;
+  func += `  return ${name} || (${name} = require('${module}'));\n}`
+  return func;
+};
+
 module.exports = (fileInfo, { jscodeshift: j }, options) => {
-  const source = j(fileInfo.source)
+  let module, name;
+
+  let source = j(fileInfo.source)
   .find(j.VariableDeclaration, {
     declarations: [{
       type: 'VariableDeclarator',
@@ -15,7 +23,20 @@ module.exports = (fileInfo, { jscodeshift: j }, options) => {
       }
     }]
   })
-  .replaceWith
+  .replaceWith(nodePath => {
+    const { node } = nodePath;
+
+    node.kind = 'let';
+    module = node.declarations[0].init.arguments[0].value || '';
+    name = node.declarations[0].id.name;
+
+    if (!module.includes('let')) {
+      nodePath.insertAfter(functionLazyReplacement(module, name));
+    }
+    node.declarations[0].init = null;
+
+    return node;
+  })
   .toSource();
 
   if (typeof options.save === 'string') {
